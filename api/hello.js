f7f2107913572d9e53ab3150b0a968d48a76c251
Uchihaobito2010 <paraschourey08@gmail.com>
2025-12-26 21:17:07 +0530

    Update name and endpoints in hello.js

--- api/hello.js
+++ api/hello.js
@@ -1,13 +1,13 @@
 module.exports = (req, res) => {
-  const YOUR_NAME = "YOUR_NAME";
+  const YOUR_NAME = "Paras Chourasiya";
   
   res.json({
     message: `Hello from ${YOUR_NAME}'s Vercel API!`,
     timestamp: new Date().toISOString(),
     endpoints: {
       scrape: "/api/scrape?url=YOUR_URL",
-      github: "/api/github?username=USERNAME"
+      github: "/api/github?username=Aotpy"
     },
-    note: "Replace YOUR_NAME with your actual name in the code"
+    note: "do not replace anything"
   });
 };
